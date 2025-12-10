3876b7334cf5cfa81d9b65dbe57e15b33e19d359
Yukimanami <ashjude03@gmail.com>
2025-12-10 11:46:00 +0800

    update latest backend1servern

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-
 const userScheduleRoutes = require('./routes/userScheduleRoutes');
-const adminSchedules = require('./routes/adminScheduleRoutes');
 
 const app = express();
 
@@ -11,17 +9,19 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
 app.use('/api/schedules', userScheduleRoutes);
+
+const adminSchedules = require('./routes/adminScheduleRoutes');
 app.use('/api/admin/schedules', adminSchedules);
+
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/reports", require("./routes/reportRoutes"));
+
+
 app.use("/api/admin", require("./routes/adminRoutes"));
 
 
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
